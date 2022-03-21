39468479e07200a2acd3914bcdb3bfee713d13b2
LamiaaAssy <lamiaa.assy@gmail.com>
2022-03-21 20:12:14 +0200

    @output

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , Input, Output } from '@angular/core';
 
 @Component({
   selector: 'events-list',
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
   <div>
   <h1>Upcoming Angular Events</h1>
   <hr />
-  <event-thumbnail [event]="event1"></event-thumbnail>
+  <event-thumbnail (eventClick)="handleEventClicked($event)"
+   [event]="event1"></event-thumbnail>
 </div>
   `
 })
@@ -24,4 +25,8 @@ export class EventsListComponent {
       country: 'England',
     },
   };
+
+  handleEventClicked(data:any ) {
+    console.log('received:', data)
+  }
 }
